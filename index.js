42a8c48479f4bc83bbe515b5524aa16329563881
lena <lena@minutemedia.com>
2023-11-15 14:44:51 +0200

    function and cong file

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 async function getList() {
-    try{
+    try {
         const response = await fetch('https://audience-boost.minutemediacdn.com/allowlist.json');
         const list = await response.json();
         return list;
     } catch (error) {
         console.error('Error fetching or parsing data:', error);
     }
-
+    return null;
 }
 const list = await getList();
 
 export const validateEmailDomain = (email, userCountry) => {
     const receivedEmailDomain = email.split('@')[1];
-    // what for?
-    if (receivedEmailDomain.length < 2) {
-        return false;
-    }
-    const isValidCountry = list.countries.filter((country) => country === userCountry);
-    const isValidDomain = list.emailDomain.filter((domain) => domain === receivedEmailDomain);
-    return (isValidDomain.length !== 0) && (isValidCountry.length !== 0);
+    if (list) {
+        const isValidCountry = list.countries.filter((country) => country === userCountry);
+        const isValidDomain = list.emailDomain.filter((domain) => domain === receivedEmailDomain);
+        return (isValidDomain.length !== 0) && (isValidCountry.length !== 0);
+    } return false;
 };
+
+// להפריד את הפונקציות
